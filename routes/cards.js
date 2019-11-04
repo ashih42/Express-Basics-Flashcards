6fd53ee9639c0bed2f3f5025dc914c8b089627fb
Andy Shih <a.shih@coldwhere.com>
2019-11-04 17:23:06 -0600

    Initial commit

---
+++ routes/cards.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+const { data } = require('../data/flashcardData.json');
+const { cards } = data;
+
+router.get('/', (req, res) => {
+  const id = Math.floor(Math.random() * cards.length);
+  res.redirect(`/cards/${id}`);
+});
+
+router.get('/:id', (req, res) => {
+  const { side } = req.query;
+  const { id } = req.params;
+  
+  if (!side) {
+    return res.redirect(`/cards/${id}?side=question`);
+  }
+  const name = req.cookies.username;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+  
+  const templateData = { id, text, name, side };
+
+  if (side === 'question') {
+    templateData.hint = hint;
+    templateData.sideToShow = 'answer';
+    // templateData.sideToShowDisplay = 'Answer';
+  } else if (side === 'answer') {
+    templateData.sideToShow = 'question';
+    // templateData.sideToShowDisplay = 'Question';
+  } else {
+    return res.redirect(`/cards/${id}?side=question`);
+  }
+  res.render('card', templateData);
+});
+
+module.exports = router;
